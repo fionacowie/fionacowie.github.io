6c03627c3519a5139f004e3ac2682bf059235eaa
Fiona-Cowie <69629937+Fiona-Co@users.noreply.github.com>
2020-09-07 14:21:17 +0100

    update layout and styling for mobile screen sizes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 console.log("Hello, welcome to my portfolio site! Hope you enjoy having a look around!");
 
-console.log("Hi!")
-
-
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
